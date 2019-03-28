3b7f6c43e6324dca445e662f247695909911d3dc
Leonardo Lemes Bufalo <leonardolsbufalo@gmail.com>
2019-03-28 15:58:23 -0300

    saw the int type

---
+++ src/variablesAndFlows/Variables.java
@@ -0,0 +1,19 @@
+package variablesAndFlows;
+
+public class Variables {
+
+    public static void main(String[] args){
+        System.out.println("Olá novo teste");
+
+        int idade;
+        idade = 37;
+        System.out.println("idade é" + idade);
+
+        idade = 30+70;
+        System.out.println(idade);
+
+        idade = (7*5) + 2;
+        System.out.println(idade);
+    }
+
+}
