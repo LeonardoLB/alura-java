825321cbff650ebab03973a30dc1caaac061bba8
Leonardo Lemes Bufalo <leonardolsbufalo@gmail.com>
2019-03-29 17:44:45 -0300

    conditiona if

---
+++ src/variablesAndFlows/Condicao.java
@@ -0,0 +1,19 @@
+package variablesAndFlows;
+
+public class Condicao {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        if (idade > 18) {
+            System.out.println("Maior que 18");
+        } else if(idade == 20) {
+            System.out.println("Tem vinte");
+        } else {
+            System.out.println("Menor que 18");
+        }
+
+
+
+    }
+
+}
