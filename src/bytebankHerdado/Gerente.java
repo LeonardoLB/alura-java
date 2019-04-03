6e4a754bfcbedfc792113d8d8921149ec9e2d42c
Leonardo Lemes Bufalo <leonardolsbufalo@gmail.com>
2019-04-03 11:01:44 -0300

    heritage

--- src/bytebankHerdado/Gerente.java
+++ src/bytebankHerdado/Gerente.java
@@ -4,6 +4,10 @@ public class Gerente extends Funcionario {
 
     private int senha;
 
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
     public boolean autentica(int senha) {
         if (senha == this.senha) {
             return true;
