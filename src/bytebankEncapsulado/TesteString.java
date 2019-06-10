6e98090b5454c1cae054f8d4e8bdae1d10d1e07a
Leonardo Lemes Bufalo <leonardolsbufalo@gmail.com>
2019-06-10 14:45:00 -0300

    String methods

--- src/bytebankEncapsulado/TesteString.java
+++ src/bytebankEncapsulado/TesteString.java
@@ -14,6 +14,23 @@ public class TesteString {
 
         nome = nome.replace("l", "L");
 
+        nome.toLowerCase(); // letra minuscula
+        nome.toUpperCase(); //  letra maiuscula
+
+        String frase = "  Frase de Teste";
+        String frase2 = frase.trim(); // remove espaco no inicio e final
+
+        String emptyFrase = " ";
+        String emptyfrase2 = emptyFrase.trim();
+
+
+        System.out.println(nome.charAt(2)); // pega a letra na posição
+
+        System.out.println("contem o conjunto de letras [este]: " + frase.contains("este"));
+        System.out.println("contem o conjunto de letras [leo]: " + frase.contains("leo"));
+        System.out.println("é vazio? " + emptyfrase2.isEmpty());
+        System.out.println("remover espacos iniciais e finais:" + frase2); // remove espacos
+
         System.out.println(nome);
 
     }
