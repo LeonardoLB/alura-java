81badd7a0f85c64d6bbcdc46653a156c3b9f4215
Leonardo Lemes Bufalo <leonardolsbufalo@gmail.com>
2019-06-11 19:42:17 -0300

    array object

--- src/Arrays/TesteArrayObject.java
+++ src/Arrays/TesteArrayObject.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import bytebankHerdado.Funcionario;
+import bytebankPackage.br.com.bytebank.banco.modelo.Cliente;
 import bytebankPackage.br.com.bytebank.banco.modelo.Conta;
 import bytebankPackage.br.com.bytebank.banco.modelo.ContaCorrente;
 import bytebankPackage.br.com.bytebank.banco.modelo.ContaPoupanca;
@@ -8,7 +10,10 @@ public class TesteArrayObject {
 
     public static void main(String[] args){
 
+        // aceita so contas como tipo conta, corrente e poupanca
         Conta[] contas = new Conta[3];
+        // aceita todos os tipo de objeto(classe)
+        Object[] objetos = new Object[1];
 
         ContaCorrente cc1 = new ContaCorrente(200, 2202);
         ContaCorrente cc2 = new ContaCorrente(100, 4301);
@@ -25,6 +30,13 @@ public class TesteArrayObject {
         System.out.println(ref.getNumero());
         System.out.println(contas[1].getNumero());
         System.out.println(refTypeCast.getNumero());
+
+        Cliente cl1 = new Cliente();
+
+        objetos[0] = cc1;
+        objetos[1] = cp1;
+        objetos[2] = cl1;
+
 
     }
 
