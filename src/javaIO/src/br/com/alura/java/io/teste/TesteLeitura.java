a53b484b8d327bfd5690ad9a5368b695a32591b2
Leonardo <leonardo.bufalo@wirecard.com>
2019-06-22 18:53:38 -0300

    input file

---
+++ src/javaIO/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,29 @@
+package javaIO.src.br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteLeitura {
+
+public static void main(String[]args) throws FileNotFoundException, IOException {
+
+        // Read a file
+        FileInputStream fis = new FileInputStream("Text.txt");
+
+        // Read a captured file
+        InputStreamReader isr = new InputStreamReader(fis);
+
+        // Help to read every words
+        BufferedReader br = new BufferedReader(isr);
+
+
+        String linha = br.readLine();
+
+        System.out.println(linha);
+
+        br.close();
+
+
+    }
+
+
+}
