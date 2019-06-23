69112eed728444de43f2550d5bd5b49ac45d61a4
Leonardo <leonardo.bufalo@wirecard.com>
2019-06-22 21:43:15 -0300

    leanerd about FileWriter

--- src/javaIO/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ src/javaIO/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -6,10 +6,12 @@ public class TesteCopiarArquivo {
 
     public static void main(String[] args) throws IOException {
 
+        // fluxo de entrada de um arquivo
         InputStream fis = System.in; //new FileInputStream("lorem.txt");
         Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
+        // fluxo de saida de um arquivo
         OutputStream fos = new FileOutputStream("Teste3.txt"); //new FileOutputStream("lorem2.txt");
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
