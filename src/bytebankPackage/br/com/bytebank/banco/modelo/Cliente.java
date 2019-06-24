cc06798ffcfddca80dea9982b9045c0cba24deec
LeonardoLB <leonardolsbufalo@gmail.com>
2019-06-24 09:15:05 -0300

    collections

--- src/bytebankPackage/br/com/bytebank/banco/modelo/Cliente.java
+++ src/bytebankPackage/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,6 +1,8 @@
 package bytebankPackage.br.com.bytebank.banco.modelo;
 
-public class Cliente {
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
 
     private String nome;
     private String cpf;
